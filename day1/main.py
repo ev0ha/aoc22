39001ee01435530d4b29e01bc16095969883ac89
ev0ha <ev091@gmx.de>
2022-12-02 08:28:19 +0100

    Refactor Day1
    Cleaned up day 1 solution

--- day1/main.py
+++ day1/main.py
@@ -1,32 +1,6 @@
 from functools import reduce
 
-def mostCal(input_data: str):
-    input = []
-
-    with open(input_data) as f:
-        [input.append(int(line.strip() or 0)) for line in f.readlines()]
-
-    foodGrouped = [[]]
-
-    i = 0
-
-    for x in input:
-        if x != 0:
-            foodGrouped[i].append(x)
-        else:
-            foodGrouped.append([])
-            i += 1
-
-    result = []
-
-    for x in foodGrouped:
-        r = reduce((lambda a, b: a + b), x, 0)
-        result.append(r)
-
-    return max(result)
-
-
-def threeMostCal(input_data: str):
+def common(input_data: str):
     input = []
 
     with open(input_data) as f:
@@ -49,12 +23,22 @@ def threeMostCal(input_data: str):
         r = reduce((lambda x, y: x + y), x, 0)
         result.append(r)
 
+    return result
+
+def problem1(input_data: str):
+    result = common(input_data)
+
+    return max(result)
+
+
+def problem2(input_data: str):
+    result = common(input_data)
     result = sorted(result)
     result.reverse()
     result = reduce((lambda x, y: x + y), result[:3], 0)
 
     return result
 
-print(mostCal('input.txt'))
-print(threeMostCal('input.txt'))
+print(problem1('input.txt'))
+print(problem2('input.txt'))
 
--- day1/test_simple.py
+++ day1/test_simple.py
@@ -1,7 +1,7 @@
-from main import mostCal, threeMostCal
+from main import problem1, problem2
 
 def test_input_simple():
-    assert mostCal('input_test.txt') == 24000
+    assert problem1('input_test.txt') == 24000
 
 def test_part_two():
-    assert threeMostCal('input_test.txt') == 45000
+    assert problem2('input_test.txt') == 45000
