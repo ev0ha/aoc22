6ceedbe81abf00b2c7b219ac47e61b25adb8c325
ev0ha <ev091@gmx.de>
2022-12-03 10:15:21 +0100

    Day 3 Solution
    Completed part two

--- day3/main.py
+++ day3/main.py
@@ -1,7 +1,7 @@
 from functools import reduce
 
 
-def char_range(char1, char2):
+def char_range(char1: str, char2: str):
     for c in range(ord(char1), ord(char2)+1):
         yield chr(c)
 
@@ -14,7 +14,7 @@ for c in char_range('A', 'Z'):
 
 charNumbers = { chars[i] : i+1 for i in range(0, len(chars) ) }
 
-def problem1(input_data: str):
+def problem1(input_data: str) -> int:
     data = []
 
     with open(input_data) as f:
@@ -25,14 +25,36 @@ def problem1(input_data: str):
     for x in data:
         str1 = x[:len(x)//2]
         str2 = x[len(x)//2:]
-        x = set(str1)&set(str2)
+        x = set(str1) & set(str2)
         result.append(charNumbers[list(x)[0]])
 
     result = reduce((lambda x, y: x + y), result, 0)
 
     return result
 
-def problem2():
-    return
+def problem2(input_data: str) -> int:
+    data = []
+
+    with open(input_data) as f:
+        [data.append(line.strip()) for line in f.readlines()]
+
+    bagsOfThree = []
+
+    for i in range(0, len(data)-1, 3):
+        group = []
+        for x in range(0, 3):
+            group.append(data[i+x])
+        bagsOfThree.append(group)
+
+    result = []
+
+    for x in bagsOfThree:
+        badge = set(x[0]) & set(x[1]) & set(x[2])
+        result.append(charNumbers[list(badge)[0]])
+
+    result = reduce((lambda x, y: x + y), result, 0)
+
+    return result
 
 print(problem1('input.txt'))
+print(problem2('input.txt'))
